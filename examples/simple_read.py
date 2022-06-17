577e89b172e16fe4e705e3883c4b5ffae9a58d4f
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-06-17 14:43:57 +1000

    Update simple_read.py

--- examples/simple_read.py
+++ examples/simple_read.py
@@ -4,5 +4,6 @@ from PiicoDev_Unified import sleep_ms
 pot = PiicoDev_Potentiometer(min=0.0, max=100.0)   # Initialise the RFID module
 
 while True:
-    print(pot.read())
-    sleep_ms(1000)
+    value = pot.read()
+    print(value)
+    sleep_ms(100)
